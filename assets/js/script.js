418db84a1ac87b75117a8fe881c8423a80785596
JG <jg.hello@proton.me>
2022-12-15 23:05:23 -0500

    added new script.js  and submit button

---
+++ assets/js/script.js
@@ -0,0 +1,9 @@
+const submitButton = document.getElementById("submit");
+submitButton.addEventListener("click", addTask);
+
+function addTask() {
+  let fakeButton = document.getElementById("submit");
+  alert("Add Task!");
+}
+
+
