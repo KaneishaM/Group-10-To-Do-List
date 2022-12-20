2300ff8633ecb1f9f4bb76b18c37dc6f7cea6f6a
JG <jg.hello@proton.me>
2022-12-19 21:55:26 -0500

    submit button  that gives an Alert message

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ const submitButton = document.getElementById("submit");
 submitButton.addEventListener("click", addTask);
 
 function addTask() {
-  let fakeButton = document.getElementById("submit");
+  
   alert("Add Task!");
 }
 
