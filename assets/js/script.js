f2d13db5cc2304c4309baa9be18e77405bb7ce73
Abiprodigy <abateb2021@gmail.com>
2023-01-11 07:45:10 -0500

    optional to-do list initial look

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,56 +1,106 @@
-//add task button
-const addTaskButton = document.getElementById("submit");
-addTaskButton.addEventListener("click", addTask);
-//clear all tasks button
-const clearButton = document
-  .getElementById("clearBtn")
-  .addEventListener("click", clearTask);
-//mark-as-complete button
-const checkButton = document
-  .getElementById("check")
-  .addEventListener("click", checkTask);
-//button for removing specific tasks
-const trashButton = document
-  .getElementById("trash")
-  .addEventListener("click", trashTask);
+todoMain();
 
-//variables for dynamic list items
-const taskItem = document.getElementById('taskItem');
-const taskDescriptionItem = document.getElementById('taskDescription');
-const assignedNameItem= document.getElementById('assignedName');
-const dateItem = document.getElementById('dateItem');
+function todoMain() {
+  let inputElem,
+  inputElem2,
+  button,
+  ulElem;
 
-//for validating task form field
-const newTaskNameInput = document.querySelector("#newTaskName");
-const taskName = newTaskNameInput.value;
-console.log("Task name:" + taskName); //displays the new task field input to the console log testing
+  getElements();
+  addListeners();
 
-const nameInput = document.querySelector("#inputName");
-const assignedName = nameInput.value;
-console.log("Assigned name:" + assignedName); //displays the assigned to field input to the console log testing
+  function getElements() {
+   inputElem = document.getElementsByTagName
+    ("input")[0];
+    inputElem2 =document.getElementsByTagName
+    ("input")[1];
+    button = document.getElementById("addBtn")
+     ulElem = document.getElementsByTagName("ul")[0];
+     console.log(ulElem);
+  }
 
-const newTaskDescription = document.querySelector("#description");
-const taskDescription = newTaskDescription.value;
-console.log("Description:" + taskDescription); //displays the task description field input to the console log testing
+  function addListeners(){
+    button.addEventListener("click", addEntry, false);
+  }
 
-const newTaskDate = document.querySelector("#date");
-const date = newTaskDate.value;
-console.log("Date:" + date); //displays selected date from field input to the console log for testing 
+  function addEntry(event){
+    let flag = true;
 
-function addTask() { //sends an alert to the browser when this button is pressed for testing
-}
-  alert("Add Task!"); 
+    let inputValue = inputElem.value;
+    inputElem.value = "";
 
-function clearTask() { //sends an alert to the browser when this button is pressed for testing
-  alert("Cleared Task");
-}
+    let inputValue2 = inputElem2.value;
+    inputElem2.value=""
 
-function checkTask() { //sends an alert to the browser when this button is pressed for testing
-  alert("Task Complete!");
- taskItem.style.setProperty("text-decoration", "line-through") //adds 'strike-through' to text decoration
-}
-function trashTask() { //sends an alert to the browser when this button is pressed for testing
+    //add a new row
+
+    let table = document.getElementById("todoTable");
+
+    let trElem = document.createElement("tr");
+    table.appendChild(trElem); //how i created a new row in table
+
+    //checkbox cell
+    let checkboxElem = document.createElement("input");
+
+    checkboxElem.type = "checkbox";
+    checkboxElem.addEventListener("click", done, false)
+    let tdElem1 = document.createElement("td");
+    tdElem1.appendChild(checkboxElem);
+    trElem.appendChild(tdElem1)
 
 
-  alert("Task Removed!");
+    //to-do cell
+    let tdElem2 = document.createElement("td");
+    tdElem2.innerText = inputValue;
+    trElem.appendChild(tdElem2);
+
+    //Assigned-to cell
+    let tdElem3 = document.createElement("td");
+    tdElem3.innerText = inputValue2;
+    trElem.appendChild(tdElem3);
+
+    //delete cell
+    let spanElem = document.createElement("span");
+    spanElem.innerText = "delete";
+    spanElem.className = "material-icons";
+    spanElem.addEventListener("click", deleteItem, false);
+    let tdElem4 = document.createElement("td");
+    tdElem4.appendChild(spanElem);
+    trElem.appendChild(tdElem4)
+
+
+
+
+
+    // let liElem = document.createElement("li");
+    // let checkboxElem = document.createElement("input");
+    // checkboxElem.type="checkbox"
+    // liElem.append(checkboxElem)
+
+
+    // let textElem =document.createElement("span")
+    // textElem.innerText = inputValue + "-" + inputValue2 ;
+    // liElem.appendChild(textElem)
+    // // liElem.innerText = inputValue;
+    // // liElem.addEventListener("click",onclick,
+    // // false);
+
+    // let spanElem = document.createElement("span");
+    // spanElem.innerText = "delete";
+    // spanElem.className = "material-icons";
+
+    // spanElem.addEventListener("click", deleteItem, false);
+
+    // liElem.appendChild(spanElem);
+
+    // ulElem.appendChild(liElem);
+
+    function deleteItem(){
+     trElem.remove();
+    }
+
+    function done(){
+      trElem.classList.toggle("strike")
+    }
+  }
 }
